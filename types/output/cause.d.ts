6a852edbf0b32d9c7ae5001bf6bf27d97fce179f
Sunjith Sukumaran <sunjith@admod.com>
2021-11-12 15:30:26 +0530

    Cause symptoms
    Cause symptoms in 'getCause'

--- types/output/cause.d.ts
+++ types/output/cause.d.ts
@@ -3,4 +3,12 @@ import { ApiOutput } from "./common";
 export interface ApiCauseOutput extends ApiOutput {
     result?: CauseOutput[];
 }
-export declare type CauseOutput = NamedItem;
+export interface CauseOutput extends NamedItem {
+    symptoms?: CauseSymptomOutput[];
+}
+interface CauseSymptomOutput extends NamedItem {
+    direct?: number;
+    specific?: number;
+    cardinal?: number;
+}
+export {};
