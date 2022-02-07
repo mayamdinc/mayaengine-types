135ce13f2a145a6f507ea3e51527fe8ba4c67d02
Sunjith Sukumaran <sunjith@admod.com>
2022-02-07 10:42:29 +0530

    Remove mongoose dependency from types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,2 @@
-import type { ConnectOptions } from "mongoose";
-export interface MongooseConfiguration {
-    uri: string;
-    options: ConnectOptions;
-}
 export * from "./input";
 export * from "./output";
